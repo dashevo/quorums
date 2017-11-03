3af14a3dc9b5e518b5c255d9c16b0ae9565d5da4
Pierre <pvrooyen@gmail.com>
2017-11-03 09:21:08 +0200

    interrim commit

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     getQuorum: require('./lib/determination').getQuorum,
     validate: require('./lib/validation').validate,
     getVin: require('./lib/utils').resolveVinFromIp,
-    getMockMnList: require('./lib/mocks').getDynamicMnList
+    getMockMnList: require('./lib/mocks').getDynamicMnList,
+    getHash: require('./lib/utils').getHash
 }
