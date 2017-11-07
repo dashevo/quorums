5da222c5db1c53d7eb581e3298cbc279f5903173
Pierre <pvrooyen@gmail.com>
2017-11-07 10:24:15 +0200

    improve config settings

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     validate: require('./lib/validation').validate,
     getVin: require('./lib/utils').resolveVinFromIp,
     getHash: require('./lib/utils').getHash,
+    getRefHeight: require('./lib/utils').getRefHeight,
     getMockMnList: require('./lib/mocks').getDynamicMnList,
-    quorumSize: require('./lib/config/').quorumSize
+    config: require('./lib/config/')
 }
