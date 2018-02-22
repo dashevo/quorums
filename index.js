a05cd3b9038a7a69904f3314e2929800e31d17da
Cofresi <cofresi@dash.org>
2018-02-22 18:27:43 -0400

    delete resolveVinFromPubkey() no use anymore since rpc cmd getMasternodeList() in Dapi now includes 'mode' and 'filter' arguments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
     getQuorum: require('./lib/determination').getQuorum,
     validate: require('./lib/validation').validate,
-    getVin: require('./lib/utils').resolveVinFromPubkey,
+    getVin: require('./lib/utils').resolveVinFromIp,
     getHash: require('./lib/utils').getHash,
     getRefHeight: require('./lib/utils').getRefHeight,
     getMockMnList: require('./lib/mocks').getDynamicMnList,
