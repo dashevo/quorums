55eb77ecf3c1355269a7199713138b9cdd445327
Cofresi <cofresi@dash.org>
2018-02-23 18:28:03 -0400

    utils: add getAverageHash(headers)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     validate: require('./lib/validation').validate,
     getVin: require('./lib/utils').resolveVinFromIp,
     getHash: require('./lib/utils').getHash,
+    getAverageHash: require('./lib/utils').getAverageHash,
     getRefHeight: require('./lib/utils').getRefHeight,
     getMockMnList: require('./lib/mocks').getDynamicMnList,
     config: require('./lib/config/')
