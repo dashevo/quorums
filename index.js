843273f523e313c67c9d0b50cef895282ab81f9a
Cofresi <cofresi@dash.org>
2018-02-23 22:55:22 -0400

    determination: getQuorums()

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     getQuorum: require('./lib/determination').getQuorum,
+    getQuorums: require('./lib/determination').getQuorums,
     validate: require('./lib/validation').validate,
     getVin: require('./lib/utils').resolveVinFromIp,
     getHash: require('./lib/utils').getHash,
