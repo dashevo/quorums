223ace3e4d03f0b1f29943785a30eaa784b053d8
Kamuela Franco <kamuela.franco@gmail.com>
2018-03-01 15:05:33 +0000

    [EV-665] Ran eslint automated fixes

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const mocks = require('./lib/mocks');
 const config = require('./lib/config');
 
 module.exports = {
-	getQuorum: determination.getQuorum,
-	getQuorums: determination.getQuorums,
-	validate: validation.validate,
-	resolveVinFromIp: utils.resolveVinFromIp,
-	getHash: utils.getHash,
-	getAverageHash: utils.getAverageHash,
-	getRefHeight: utils.getRefHeight,
-	getHeartBeatInterval: utils.getHeartBeatInterval,
-	getDynamicMnList: mocks.getDynamicMnList,
-	getPOWAveragingNum: utils.getPOWAveragingNum,
-	getConfig: config.getConfig
-}
+  getQuorum: determination.getQuorum,
+  getQuorums: determination.getQuorums,
+  validate: validation.validate,
+  resolveVinFromIp: utils.resolveVinFromIp,
+  getHash: utils.getHash,
+  getAverageHash: utils.getAverageHash,
+  getRefHeight: utils.getRefHeight,
+  getHeartBeatInterval: utils.getHeartBeatInterval,
+  getDynamicMnList: mocks.getDynamicMnList,
+  getPOWAveragingNum: utils.getPOWAveragingNum,
+  getConfig: config.getConfig,
+};
